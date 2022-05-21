c2ad09acb3b476b5fc5715d0cf582b27fd251254
AC1556 <allenclark@u.boisestate.edu>
2022-05-21 14:22:56 +0900

    Logos and base stylings

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,21 @@
 <script setup>
-  // This starter template is using Vue 3 <script setup> SFCs
-  // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-  // import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
-    <h1>Currently under development 👩‍💻</h1>
+    <h1> Urban Conversion </h1>
+    <img alt="Urban Conversion Logo" src="./assets/logo.png" />
+    <h2>Currently under development 👩‍💻</h2>
   </div>
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
 </template>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+@import "./assets/styles/index.css";
+
+body {}
+
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
 </style>
