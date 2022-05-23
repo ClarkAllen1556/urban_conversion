4c1d8225bee13ed86677dd7bf5085fc1eac27ad6
AC1556 <allenclark@u.boisestate.edu>
2022-05-23 18:24:45 +0900

    Installing/Adding tailwind

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import './assets/styles/index.css';
+
+if (
+  localStorage.theme === 'dark' ||
+  (!('theme' in localStorage) &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark');
+}
 
 createApp(App).mount('#app');
