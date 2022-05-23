4c8fe15397509d2f73f50bd97876f2de5b1a2e94
AC1556 <allenclark@u.boisestate.edu>
2022-05-23 23:00:18 +0900

    Adding dark mode toggle button

--- src/components/Button.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-
-<template>
-  <button @click="$emit('btn-click')">
-    <slot />
-  </button>
-</template>
---
+++ src/components/ThemeButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  theme: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <button @click="() => $emit('toggle-theme')">
+    <div v-if="theme === 'light'">
+      <img src="src/assets/moon_icon.svg" width="30" height="30" />
+    </div>
+    <div v-else>
+      <img src="src/assets/sun_icon.svg" width="30" height="30" />
+    </div>
+  </button>
+</template>
